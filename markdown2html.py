2e21a2be8b96c730171571aaf46fbfeed5b6a3f6
AyariSiwar <91055926+Siwarayari@users.noreply.github.com>
2023-06-16 12:08:21 +0200

    script markdown2html.py that takes an argument 2 strings

--- markdown2html.py
+++ markdown2html.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 # take argument 2 strings argument is the name of the Markdown file and argument is the output file name
-"""take argument 2 strings"""
+"""script markdown2html.py that takes an argument 2 strings"""
 import markdown
 import sys
 
 
-# take argument 2 strings
-"""take argument 2 strings"""
+# script markdown2html.py that takes an argument 2 strings
+"""script markdown2html.py that takes an argument 2 strings"""
 if len(sys.argv) != 3:
-    """take argument 2 strings"""
+    """script markdown2html.py that takes an argument 2 strings"""
     print("Usage: ./markdown2html.py README.md README.html")
     sys.exit(1)
 
@@ -16,19 +16,19 @@ first = sys.argv[1]
 sec = sys.argv[2]
 
 try:
-    # take argument 2 strings
-    """take argument 2 stringss"""
+    # script markdown2html.py that takes an argument 2 strings
+    """script markdown2html.py that takes an argument 2 strings"""
     with open(first, 'r') as f:
         tempMd = f.read()
 except FileNotFoundError:
-    # take argument 2 strings
-    """take argument 2 strings"""
+    # script markdown2html.py that takes an argument 2 strings
+    """script markdown2html.py that takes an argument 2 strings"""
     print("Missing {} ".format(first))
     sys.exit(1)
 
 textmark = markdown.markdown(tempMd)
 
 with open(sec, 'w') as f:
-    # take argument 2 strings
-    """take argument 2 strings"""
+    # script markdown2html.py that takes an argument 2 strings
+    """script markdown2html.py that takes an argument 2 strings"""
     f.write(textmark)
